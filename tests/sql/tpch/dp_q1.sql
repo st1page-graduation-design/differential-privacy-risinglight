1748defa6208281bdb8eba1b98bea0f6e0307f82
st1page <1245835950@qq.com>
2022-05-25 01:59:18 +0800

    remove some agg in dp q1

--- tests/sql/tpch/dp_q1.sql
+++ tests/sql/tpch/dp_q1.sql
@@ -2,8 +2,6 @@ select
     l_returnflag,
     l_linestatus,
     dp_sum(l_quantity                                      , 0.5) as sum_qty,
-    dp_sum(l_extendedprice                                 , 0.5) as sum_base_price,
-    dp_sum(l_extendedprice * (1 - l_discount)              , 0.5) as sum_disc_price,
     dp_sum(l_extendedprice * (1 - l_discount) * (1 + l_tax), 0.5) as sum_charge,
     dp_count(1                                             , 0.5) as count_order
 from
